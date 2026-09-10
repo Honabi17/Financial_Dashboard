3f21f70fea090b6d9e5efb4ac32a3d91148641ac
Honabi17 <luissantos1996@live.com.pt>
2026-09-10 23:55:04 +0100

    feat: Create GlobalHandlerException

---
+++ BackEnd/financial_backend/src/main/java/com/luis/financial_backend/common/exception/GlobalHandlerException.java
@@ -0,0 +1,95 @@
+package com.luis.financial_backend.common.exception;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@RestControllerAdvice
+public class GlobalHandlerException {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(
+            MethodArgumentNotValidException exception
+    ){
+        Map<String, String> errors =
+                exception
+                        .getBindingResult()
+                        .getFieldErrors()
+                        .stream()
+                        .collect(
+                            Collectors.toMap(
+                                    FieldError::getField,
+                                    FieldError::getDefaultMessage,
+                                    (a,b) -> a)
+                        );
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(
+            ConstraintViolationException exception
+    ){
+        Map<String, String> errors = new HashMap<>();
+        exception
+                .getConstraintViolations()
+                .forEach(
+                        cv -> errors.put(
+                                cv.getPropertyPath().toString(),
+                                cv.getMessage()
+                        )
+                );
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(
+            IllegalArgumentException exception
+    ){
+        Map<String, String> error = Map.of("error", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleException(
+            Exception exception
+    ){
+        Map<String, String> error = Map.of("error", "Internal server error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(
+            BadRequestException exception,
+            HttpServletRequest request
+    ){
+            ErrorResponse error =new ErrorResponse(
+                    HttpStatus.BAD_REQUEST.value(),
+                    exception.getMessage(),
+                    request.getRequestURI()
+            );
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
+
+
+
+
