a9fe09ce5cf803e3bb30d52fbb950ddfb5a1b64a
Honabi17 <luissantos1996@live.com.pt>
2026-09-11 23:43:22 +0100

    refactor: User entity update

--- BackEnd/financial_backend/src/main/java/com/luis/financial_backend/modules/auth/entity/User.java
+++ BackEnd/financial_backend/src/main/java/com/luis/financial_backend/modules/auth/entity/User.java
@@ -2,9 +2,15 @@ package com.luis.financial_backend.modules.auth.entity;
 
 
 import com.luis.financial_backend.common.BaseEntity;
+import com.luis.financial_backend.modules.person.entity.Person;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 
@@ -15,8 +21,9 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User  extends BaseEntity {
+public class User  extends BaseEntity implements UserDetails {
 
+    private String username;
 
     private String email;
 
@@ -29,4 +36,37 @@ public class User  extends BaseEntity {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles;
+
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL
+    )
+    private List<Person> persons = new ArrayList<>();
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
